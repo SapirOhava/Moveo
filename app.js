3f91051e020a2b14915498618fbb293429ca15ee
sapir ohava <sapirlove24@gmail.com>
2021-10-23 22:23:58 +0300

    basic game

---
+++ app.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(3000, () => {
+    console.log('serving on port 3000');
+})
---
+++ public/javascripts/sol.js
@@ -0,0 +1,158 @@
+const s3 = require('aws-sdk/clients/s3')
+const playButton = document.querySelector('button');
+const container = document.querySelector('#pads');
+let TracksToAddToCycle = [];
+
+const files = ["music/1.mp3", "music/2.mp3", "music/3.mp3",
+    "music/4.mp3", "music/5.mp3", "music/6.mp3", "music/7.mp3", "music/8.mp3",
+    "music/9.mp3"
+];
+
+const audios = files.map(file => new Audio(file));
+var ctx = new AudioContext();
+const merger = ctx.createChannelMerger(audios.length);
+merger.connect(ctx.destination);
+
+const gains = audios.map(audio => {
+    var gain = ctx.createGain();
+    var source = ctx.createMediaElementSource(audio);
+    source.connect(gain);
+    gain.connect(merger);
+    return gain;
+});
+for (let i = 0; i < gains.length; i++) {
+    gains[i].gain.value = 0;
+}
+//all gains values are o
+let isSomeGainIsOn = 'false';
+
+// buffers values sais if all audio are downloaded and ready to be played
+let buffered = 'false';
+// num of files are not loaded yet
+let load = files.length;
+audios.forEach(audio => {
+    audio.addEventListener("canplaythrough", () => {
+        console.log(audio.attributes.src.value);
+        load--;
+        if (load === 0) {
+            buffered = 'true';
+        }
+    });
+})
+
+//return the delay of all audios
+// const getDelay = function() {
+//     var times = [];
+//     for (var i = 0; i < audios.length; i++) {
+//         times.push(audios[i].currentTime);
+//     }
+
+//     var minTime = Math.min.apply(Math, times);
+//     var maxTime = Math.max.apply(Math, times);
+//     return maxTime - minTime;
+// }
+
+// //set the same time to all audios
+// const setTime = function(time) {
+//     audios.forEach(audio => audio.currentTime = time);
+// }
+
+// setInterval(() => {
+//     if (getDelay() >= 0.05) {
+//         setTime(audios[0].currentTime);
+//     }
+// }, 5000);
+
+
+playButton.addEventListener('click', function() {
+
+    // check if context is in suspended state (autoplay policy)
+    if (ctx.state === 'suspended') {
+        ctx.resume();
+    }
+
+    // play or pause track depending on state
+    //play
+    if (this.dataset.playing === 'false') {
+        console.log(`playbutton pressed, TracksToAddToCycle=${TracksToAddToCycle}`);
+        isSomeGainIsOn = 'false';
+        for (let i = 0; i < gains.length; i++) {
+            if (gains[i].gain.value === 1) {
+                isSomeGainIsOn = 'true';
+            }
+        }
+        //if every gain is with 0 vol , then we turn on the audio pad immediatly
+        if (isSomeGainIsOn === 'false') {
+            for (let i = 0; i < TracksToAddToCycle.length; i++) {
+                gains[TracksToAddToCycle[i]].gain.value = 1;
+            }
+
+            if (TracksToAddToCycle.length > 0) {
+                audios.forEach(audio => audio.play());
+                //removes all pads from the array, because weve already unmuted their gain nodes
+                TracksToAddToCycle = [];
+                this.dataset.playing = 'true';
+            }
+
+        }
+        //if at least one gain is with vol 1 , the pad is on the next cycle
+        if (isSomeGainIsOn === 'true') {
+            audios.forEach(audio => audio.play());
+            this.dataset.playing = 'true';
+        }
+
+
+    } //pause
+    else if (this.dataset.playing === 'true') {
+
+        audios.forEach(audio => audio.pause());
+        for (let i = 0; i < gains.length; i++) {
+            gains[i].gain.value = 0;
+        }
+        this.dataset.playing = 'false';
+    }
+
+}, false);
+
+
+
+
+//when the cycle end , and a new cycle is starting
+audios[0].addEventListener('ended', () => {
+    console.log(`ended, TracksToAddToCycle=${TracksToAddToCycle}`);
+    for (let i = 0; i < TracksToAddToCycle.length; i++) {
+        gains[TracksToAddToCycle[i]].gain.value = 1;
+    }
+
+    const doStuff = function() {
+        if (buffered !== 'true') { //we want it to match to true
+            setTimeout(doStuff, 50); //wait 50 millisecnds then recheck
+            return;
+        }
+    }
+    doStuff();
+    if (buffered === 'true') {
+        audios.forEach(audio => audio.play());
+    }
+
+    //removes all pads from the array, because weve already unmuted their gain nodes
+    TracksToAddToCycle = [];
+}, false);
+
+// adds a number of the pad to the TracksToAddToCycle array
+const addTrackToCycle = function(numOfTrack) {
+    TracksToAddToCycle.push([parseInt(numOfTrack, 10)]);
+}
+
+// when a pad is clicked
+container.addEventListener('click', function(e) {
+    if (e.target.attributes.state.value === 'false') {
+        const trackNumber = e.target.attributes.id.value;
+        addTrackToCycle(trackNumber);
+        e.target.attributes.state.value = 'true';
+    } else {
+        const trackNumber = e.target.attributes.id.value;
+        gains[parseInt(trackNumber, 10)].gain.value = 0;
+        e.target.attributes.state.value = 'false';
+    }
+})
