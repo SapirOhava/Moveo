161c54e53bcf606e13bb380417a299c3b30423e6
sapir ohava <sapirlove24@gmail.com>
2021-10-23 23:59:51 +0300

    fetching the songs from aws s3 server

--- public/javascripts/audioMer.js
+++
@@ -1,8 +0,0 @@
-const AudioMerger = require('./AudioMerger');
-const playButton = document.querySelector('button');
-const container = document.querySelector('#pads');
-
-const files = ["music/1.mp3", "music/2.mp3", "music/3.mp3",
-    "music/4.mp3", "music/5.mp3", "music/6.mp3", "music/7.mp3", "music/8.mp3",
-    "music/9.mp3"
-];
--- public/javascripts/sol.js
+++ public/javascripts/sol.js
@@ -3,9 +3,15 @@ const playButton = document.querySelector('button');
 const container = document.querySelector('#pads');
 let TracksToAddToCycle = [];
 
-const files = ["music/1.mp3", "music/2.mp3", "music/3.mp3",
-    "music/4.mp3", "music/5.mp3", "music/6.mp3", "music/7.mp3", "music/8.mp3",
-    "music/9.mp3"
+const files = ["https://9pads-songs.s3.us-east-2.amazonaws.com/1.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/2.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/3.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/4.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/5.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/6.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/7.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/8.mp3",
+    "https://9pads-songs.s3.us-east-2.amazonaws.com/9.mp3"
 ];
 
 const audios = files.map(file => new Audio(file));
